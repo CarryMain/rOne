3046a722a7d8f238faecd2cdfcce53f728ad76fe
CarryMain <mr.turanov98@gmail.com>
2023-10-26 18:36:32 +0300

    'react'

---
+++ users/src/components/app/app.js
@@ -0,0 +1,32 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function App() {
+
+    const data = [
+        {name:"Kirill A." , salary: 1000,  increase: false},
+        {name:"Nataliya A." , salary: 6000,  increase: true},
+        {name:"John A." , salary: 55000, increase: false }
+    ];
+    
+  return (
+    <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+            <SearchPanel/>
+            <AppFilter/>
+        </div>
+        
+        <EmployeesList data={data}/>
+        <EmployeesAddForm/>
+    </div>
+  );
+}
+
+export default App;
